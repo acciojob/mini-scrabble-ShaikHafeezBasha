0e993dcbe222ee94688775627bb9a8746a6e57fd
Shaik Hafeez Basha <115686162+ShaikHafeezBasha@users.noreply.github.com>
2023-08-16 23:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your code here
+let h3 = document.getElementById("letterCount")
+let input = document.getElementById("evaluatedText")
+
+input.addEventListener('input', () => {
+    h3.innerText = `${input.value.length}`
+})
